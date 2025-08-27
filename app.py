1cf07df8065ce5b7817d9b64bfaf45ca01848893
Davi Garcia Machado Janusz <24011638@fecap.br>
2025-08-27 12:17:26 -0300

    passagens

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/passagens')
+def passagens():
+    return render_template('passagens.html')
+
 if __name__ == '__main__':
     app.run(debug = True)
